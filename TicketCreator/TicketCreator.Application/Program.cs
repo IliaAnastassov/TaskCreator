d0cef767855308a5e56e62c6b91fe438e7339b7d
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-05-25 17:39:03 +0300

    Add version 1.0.0.1 of the app

---
+++ TicketCreator/TicketCreator.Application/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace TicketCreator.Application
+{
+    public class Program
+    {
+        const char HASH = '#';
+        const string INFO_PREFIX = "info_";
+        const string QUERIES_PREFIX = "queries_";
+        const string EXTENSION_TXT = ".txt";
+        const string EXTENSION_SQL = ".sql";
+
+        public static void Main()
+        {
+            try
+            {
+                var ticketId = Console.ReadLine();
+                var currentDirPath = Directory.GetCurrentDirectory();
+                var ticketDirPath = Path.Combine(currentDirPath, $"{HASH}{ticketId}");
+
+                if (!Directory.Exists(ticketDirPath))
+                {
+                    Directory.CreateDirectory(ticketDirPath);
+
+                    CreateFile(INFO_PREFIX, ticketId, ticketDirPath, EXTENSION_TXT);
+                    CreateFile(QUERIES_PREFIX, ticketId, ticketDirPath, EXTENSION_SQL);
+                }
+                else
+                {
+                    Console.WriteLine($"A directory for ticket {HASH}{ticketId} already exists!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+
+        private static void CreateFile(string prefix, string ticketId, string ticketDirPath, string extension)
+        {
+            var infoFileName = $"{prefix}{ticketId}{extension}";
+            var infoFilePath = Path.Combine(ticketDirPath, infoFileName);
+
+            if (!File.Exists(infoFilePath))
+            {
+                File.Create(infoFilePath);
+            }
+        }
+    }
+}
